13a092929994c7df1ee4e88efd898aa60daa7ae2
Paweł Protaś <pawciopr@gmail.com>
2024-12-08 10:58:28 +0100

    Add implementation of fibonacci_recursive for any natural number

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -21,7 +21,9 @@ int fibonacci_iterative(int sequence) {
 int fibonacci_recursive(int sequence) {
     if (sequence == 0) {
         return 0;
-    } else {
+    }
+    if (sequence == 1) {
         return 1;
     }
+    return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
