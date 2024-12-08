5a9ac7d6e031eefe87436f51cd9299264fb5c5d1
Paweł Protaś <pawciopr@gmail.com>
2024-12-08 09:48:19 +0100

    Add implementation to calculate function for add command

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,5 +2,8 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
+    if (command == "add") {
+        return std::to_string((first + second));
+    }
     return "Invalid data";
 }
