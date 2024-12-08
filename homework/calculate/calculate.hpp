29a8af478be26c395640b1eb3621ad949aac3c12
Paweł Protaś <pawciopr@gmail.com>
2024-12-08 09:59:11 +0100

    Add implementation to calculate function for subtract command

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,9 +1,12 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) {
+std::string calculate(const std::string &command, int first, int second) {
     if (command == "add") {
-        return std::to_string((first + second));
+        return std::to_string(first + second);
+    } else if (command == "subtract") {
+        return std::to_string(first - second);
+    } else {
+        return "Invalid data";
     }
-    return "Invalid data";
 }
