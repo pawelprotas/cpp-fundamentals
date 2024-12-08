4b649c0e3c6a384f8ad11453655fd1ebe948327a
Paweł Protaś <pawciopr@gmail.com>
2024-12-08 14:05:51 +0100

    Add implementation of NWD function

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,19 @@
 #pragma once
+#include <cstdlib>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (rhs == 0) {
+        return lhs;
+    }
+
+    int remainderOfDivision = lhs % rhs;
+    while (remainderOfDivision != 0) {
+        lhs = rhs;
+        rhs = remainderOfDivision;
+        remainderOfDivision = lhs % rhs;
+    }
+
+    return abs(rhs);
 }
 
 int NWW(int lhs, int rhs) {
