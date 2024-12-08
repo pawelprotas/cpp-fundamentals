d1ad7cd3df963fcf46520c21b3acd3089cceb330
Paweł Protaś <pawciopr@gmail.com>
2024-12-08 09:43:44 +0100

    calculate function return always string Invalid data

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,5 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    return "Invalid data";
 }
