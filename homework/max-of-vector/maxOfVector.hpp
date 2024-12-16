947be21c49d547251313f9785a896be0aeaaa628
Paweł Protaś <pawciopr@gmail.com>
2024-12-16 16:51:09 +0100

    Implement maxOfVector function with empty check

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,16 @@
 #pragma once
-#include <limits>
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    if (vec.empty()) {
+        throw std::invalid_argument("Vector cannot be empty");
+    }
+    int result = vec[0];
+    const size_t vectorSize = vec.size();
+    for (int i = 1; i < vectorSize; i++) {
+        if (vec[i] > result) {
+            result = vec[i];
+        }
+    }
+    return result;
 }
