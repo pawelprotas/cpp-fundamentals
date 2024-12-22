98e036207368b5c8dd5c9282732d403442861a33
Paweł Protaś <pawciopr@gmail.com>
2024-12-22 14:14:10 +0100

    Declare functions and add default implementation

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,14 @@
+﻿#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> result = {};
+    return result;
+}
+
+void print(std::vector<std::shared_ptr<int>> vector) {}
+
+void add10(std::vector<std::shared_ptr<int>> vector) {}
+
+void sub10(int* const ptr) {}
+
+void sub10(std::vector<std::shared_ptr<int>> vector) {}
